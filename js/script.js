f572908fc025b1430c261bc5d19cc18714b1a76e
Lia Kilmas <89234929+sutset@users.noreply.github.com>
2022-06-26 14:47:17 +0700

    update data

--- js/script.js
+++ js/script.js
@@ -2,24 +2,25 @@ function searchMovie(){
 
     $('#movie-list').html('');
 
-
+    
     $.ajax({
         url:'http://omdbapi.com',
         type: 'get',
-        dataType: 'json',
-        data: {
-            'apikey': '4afabd71',
-            's': $('#search-input').val()
+        dataType: 'json', //mengambil nilai dlm bentuk json
+        data: { // mengirim data dngn 2 parameter 
+            'apikey': '4afabd71', 
+            's': $('#search-input').val() /*'s' parameter pencarian dengn judul, jquery memanggil  element search input untuk diambil
+                                             nilainya  */ 
         },
-        success: function(result){
+        success: function(result){ //ketika ajax di kirim & ssek akan menjalankan funtion
           if (result.Response == "True"){
 
-            let movies = result.Search; 
+            let movies = result.Search; //variable movie untuk mengambil movie aja/mengambil data 1 1|langsung ke array of objek
 
-            $.each(movies, function(i, data){
-                $('#movie-list').append(`
+            $.each(movies, function(i, data){ // loping /*append berfungsi untuk menambahkan sebuah elemen*/
+                $('#movie-list').append(` 
                     <div class="col-md-4">
-                        <div class="card mb-3">
+                        <div class="card mb-3"> 
                             <img src="`+ data.Poster +`" class="card-img-top" >
                             <div class="card-body">
                                 <h5 class="card-title">`+ data.Title+`</h5>
@@ -35,7 +36,8 @@ function searchMovie(){
             $('#search-input').val('');
 
           }else{
-             $('#movie-list').html('<h1 class="text-center">Movie Not Found!</h1>')
+             $('#movie-list').html('<h1 class="text-center">Movie Not Found!</h1>')/*jquery memanggil elemen movie list & mengambil data html
+             dimana isinya movie not found */
           }
 
         }
@@ -44,7 +46,7 @@ function searchMovie(){
 }
 
 
-$('#search-button').on('click', function(){
+$('#search-button').on('click', function(){ //
     searchMovie();
 
 });
